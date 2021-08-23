cd581f0336b9d855d67399a411aa844cbf8c39c4
André Simiquelli <andresimiquelli@hotmail.com>
2021-08-23 09:12:31 -0300

    Finalização

--- index.php
+++ index.php
@@ -5,79 +5,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Caramelo - Doces Artesanais</title>
+    <link rel="stylesheet" href="css/page.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/contato.css">
+    <link rel="stylesheet" href="css/home.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@300;400;700&display=swap" rel="stylesheet">
-    <style>
-        body {
-            margin: 0;
-            font-family: 'Bitter', serif;
-        }
-
-        a {
-            text-decoration: none;
-            color: inherit;
-        }
-
-        header {
-            height: 100px;
-            background-color: #DD6A00;
-            text-align: center;
-            padding: 10px;
-        }
-
-        .logo {
-           height: 80px; 
-        }
-
-        nav {
-            background-color: #FFAD93;
-        }
-
-        nav ul {
-            list-style: none;
-            width: 100%;
-            margin: 0px;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-        }
-
-        nav ul li {
-            padding-top: 12px;
-            padding-bottom: 12px;
-            padding-left: 25px;
-            padding-right: 25px;
-        }
-
-        .content{
-            min-height: 400px;
-        }
-
-        footer{
-            background-color: #FFAD93;
-            min-height: 60px;
-            padding: 40px;
-        }
-
-        .container{
-            max-width: 800px;
-            display: flex;
-            margin: auto;
-            padding-top: 30px;
-            flex-wrap: wrap;
-        }
-    </style>
 </head>
 <body>
     <header>
-        <img src="images/logo_white.png" alt="Logo" class="logo">
+        <a href="index.php">
+            <img src="images/logo_white.png" alt="Logo" class="logo">
+        </a>        
     </header>
     <nav>
         <ul>
-            <li>Home</li>
-            <li>Quem somos</li>
-            <li>Contato</li>
+            <li>
+                <a href="index.php">Home</a>
+            </li>
+            <li>
+                <a href="index.php?page=quem_somos">Quem somos</a>
+            </li>
+            <li>
+                <a href="index.php?page=contato">Contato</a>    
+            </li>
             <li>Localização</li>
         </ul>
     </nav>
--- pages/home.php
+++ pages/home.php
@@ -1,22 +1,6 @@
 <?php
     require_once('dados/produtos.php');
-?>
-
-<style>
-    .card-produto{
-        width: 200px;
-        text-align: center;
-        margin: 30px;
-    }
-
-    .card-produto img{
-        width: 200px;
-        height: 200px;
-        border-radius: 100px;
-    }
-</style>
-
-<?php
+    
     foreach($produtos as $id => $produto):
 ?>
 
